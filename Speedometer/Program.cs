0607c42114ca21cc199d0daafd47e1fa3f53bbc3
KalerKusik <karel.kuusik1@gmail.com>
2021-09-28 09:05:49 +0300

    lol

---
+++ Speedometer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Speedometer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*The program asks for distance and time as input and displays the speed in kilometers per hour*/
+            Console.WriteLine("Enter covered distance in km: ");
+            int speed = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter time spent in hours: ");
+            int time = Int32.Parse(Console.ReadLine());
+
+            int kph = speed / time;
+            Console.WriteLine($"The km/h are: {kph}");
+
+
+        }
+    }
+}
